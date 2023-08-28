b52ac49757ecf9bea30891e0e3454a1e21e55a3f
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-28 15:37:35 +0500

    Added: add base global components for reuseable functionalities with the help of slots

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,24 +3,19 @@
         Filter
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item
-                v-for="coach in filteredCoaches"
-                :key="coach.id"
-                :id="coach.id"
-                :first-name="coach.firstName"
-                :last-name="coach.lastName"
-                :rate="coach.hourlyRate"
-                :areas="coach.areas"
-            >
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas">
 
-            </coach-item>
-        </ul>
-        <h3 v-else>No coaches found.</h3>
+                </coach-item>
+            </ul>
+            <h3 v-else>No coaches found.</h3>
+        </base-card>
     </section>
 </template>
 
@@ -30,7 +25,7 @@ import CoachItem from '@/components/coaches/CoachItem.vue'
 
 export default {
     components: {
-        'coach-item':CoachItem
+        'coach-item': CoachItem
     },
     computed: {
         filteredCoaches() {
