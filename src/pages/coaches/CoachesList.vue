99c2296032e7cc3b8235bec87b5e28a77027576d
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-29 00:55:37 +0500

    Added: add actegories of filtering coach

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        Filter
+        <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
     <section>
         <base-card>
@@ -21,18 +21,48 @@
 
 <script>
 import CoachItem from '@/components/coaches/CoachItem.vue'
-
+import CoachFilter from '@/components/coaches/CoachFilter.vue'
 
 export default {
     components: {
-        'coach-item': CoachItem
+        'coach-item': CoachItem,
+        'coach-filter': CoachFilter
+    },
+    data() {
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true
+            }
+        }
     },
     computed: {
         filteredCoaches() {
-            return this.$store.getters['coaches/coaches']
+            const coaches = this.$store.getters['coaches/coaches'];
+            return coaches.filter((coach) => {
+                if(this.activeFilters.frontend && coach.areas.includes('frontend')){
+                    return true;
+                }
+
+                if(this.activeFilters.backend && coach.areas.includes('backend')){
+                    return true;
+                }
+
+                if(this.activeFilters.career && coach.areas.includes('career')){
+                    return true;
+                }
+                
+                return false;
+            })
         },
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches']
+        }
+    },
+    methods: {
+        setFilters(updatedFilters) {
+            this.activeFilters = updatedFilters;
         }
     }
 }
