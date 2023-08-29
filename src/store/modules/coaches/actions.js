a11a1a33986ed35e87d413c074fc8e5e19865b9b
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-29 20:03:41 +0500

    Added: fetching coaches data from firebase using HTTP GET request via action and getters of coaches module

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -16,7 +16,7 @@ export default {
 
         // const responseData = await response.json();
 
-        if(!response.ok) {
+        if (!response.ok) {
             // error...
         }
 
@@ -24,5 +24,29 @@ export default {
             ...coachData,
             id: userId
         });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(`https://vue-http-demo-80b99-default-rtdb.firebaseio.com/coaches.json`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // ...
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
     }
 };
