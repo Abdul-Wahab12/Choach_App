8b03027618433fe941a8a98368cbc37e65e8a158
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-30 04:06:41 +0500

    Added: add transition to components

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,25 +1,23 @@
 <template>
-    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
-        <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-        <base-card>
-            <header>
-                <h2>Requests Received</h2>
-            </header>
-            <base-spinner v-if="isLoading"></base-spinner>
-            <ul v-else-if="hasRequests && !isLoading">
-                <request-item 
-                    v-for="req in receivedRequests" 
-                    :key="req.id"
-                    :email="req.userEmail"
-                    :message="req.message"
-                >
-            </request-item>
-            </ul>
-            <h3 v-else>You haven't received any requests yet!</h3>
-        </base-card>
-    </section>
+    <div>
+        <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Requests Received</h2>
+                </header>
+                <base-spinner v-if="isLoading"></base-spinner>
+                <ul v-else-if="hasRequests && !isLoading">
+                    <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail"
+                        :message="req.message">
+                    </request-item>
+                </ul>
+                <h3 v-else>You haven't received any requests yet!</h3>
+            </base-card>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -40,14 +38,14 @@ export default {
         receivedRequests() {
             return this.$store.getters['requests/requests']
         },
-        hasRequests(){
+        hasRequests() {
             return this.$store.getters['requests/hasRequests'];
         }
     },
     methods: {
         async loadRequests() {
             this.isLoading = true;
-            try{
+            try {
                 await this.$store.dispatch('requests/fetchRequests');
             } catch (error) {
                 this.error = error.message || 'Something failed!'
