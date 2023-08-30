730f3922075482c94348524e04449ebfd80fbddb
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-30 14:00:35 +0500

    Added: add User auth component for authentication purpose

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import CoachRegistration from '../pages/coaches/CoachRegistration.vue';
 import ContactCoach from '../pages/requests/ContactCoach.vue';
 import RequestsReceived from '../pages/requests/RequestsReceived.vue';
 import NotFound from '../pages/NotFound.vue';
+import UserAuth from '../pages/auth/UserAuth.vue';
 
 const routes = [
   {
@@ -38,6 +39,11 @@ const routes = [
     path: '/requests',
     name: 'requestsReceived',
     component: RequestsReceived
+  },
+  {
+    path: '/auth',
+    name: 'userAuth',
+    component: UserAuth
   },
   {
     path: '/:notFound(.*)',
